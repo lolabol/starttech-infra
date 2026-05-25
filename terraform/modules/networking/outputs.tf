e213493bf3f2ac3cb501c4fdc9507d30d4553aff
OmololaEyamuku <lolaeyanuku@yahoo.com>
2026-05-25 11:07:02 +0100

    Correct outputs.tf to fix terraform validate

--- terraform/modules/networking/outputs.tf
+++ terraform/modules/networking/outputs.tf
@@ -17,8 +17,3 @@ output "alb_sg_id" {
   description = "ALB security group ID"
   value = aws_security_group.alb.id
 }
-
-output "ec2_sg_id" {
-  description = "EC2 security group ID"
-  value = aws_security_group.ec2.id
-}
